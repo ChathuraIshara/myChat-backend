a0ebdc36b0a7a901eda76227394adcf41e4e85cb
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2024-07-18 09:56:19 +0530

    swagger ui uncommented

--- ChatApp/Program.cs
+++ ChatApp/Program.cs
@@ -36,12 +36,12 @@ builder.Services.AddCors(options => {
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
+ Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
-//}
+}
 
 
 
